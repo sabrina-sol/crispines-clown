3145162810b8240d20b477ec3cf5da4162b01efe
Sabrina Sol Lattner <sabri.latt@gmail.com>
2022-10-29 17:11:02 -0300

    full responsive

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 const navToogle = document.querySelector("#navToogle");
 const menu = document.getElementsByClassName("containerHeader");
 const navIcon = document.querySelectorAll(".navIcon");
+const navLi = document.querySelectorAll("li");
+const scrollTop = document.getElementById("scrollTop");
+
+/* --------------------- Open and Close Responsive Menu --------------------- */
 
 navToogle.addEventListener ("click", () => {
     menu[0].classList.toggle("openMenu");
     navIcon.forEach(icon => {
         icon.classList.toggle("hidden");
     })
+    scrollTop.classList.toggle("hideScroll");
 });
+
+/* ---------------------- Close menu after click in li ---------------------- */
+
+for(let navLink of navLi) {
+    navLink.addEventListener ("click", () => {
+        menu[0].classList.remove("openMenu");
+        navIcon.forEach(icon => {
+            icon.classList.toggle("hidden");
+        })
+    });
+};
+
+/* ------------------- Hide scroll-top btn when open menu ------------------- */
+
+
+
+/*if (menu[0].classList("openMenu")) {
+    scrollTop.classList.toggle("hidden")
+}
+
+navToogle.addEventListener ("click", () => {
+    menu[0].classList.toggle("openMenu");
+    navIcon.forEach(icon => {
+        icon.classList.toggle("hidden");
+    })
+});*/
+
+
 
